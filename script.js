6e867334a5ca69bffc816344f738cbcd8ea431a2
markie816 <markrennenberg@hotmail.com>
2023-08-08 17:48:33 +0200

    Made the header with dropdown

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* The map */
 var map = L.map('map').setView([50.95956, 5.825261], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -6,3 +7,25 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 var marker = L.marker([50.95956, 5.825261]).addTo(map);
+
+/* Open dropdown */
+function openDropdown() {
+    document.getElementById('myDropdown').classList.toggle('show');
+}
+
+// Close the dropdown menu if the user clicks outside of it
+// https://www.w3schools.com/howto/howto_js_dropdown.asp    --> LINK WAAR IK HET VANDAAG HEB
+window.onclick = function(event) {
+    if (!event.target.matches('.menu_container')) {
+      var dropdowns = document.getElementsByClassName(".dropdown-content"); //Zou zonder . moeten werken, werkt echter alleen met
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+
